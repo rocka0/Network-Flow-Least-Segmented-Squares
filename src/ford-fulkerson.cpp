b66c2e48f9bbec00789b8c9d3bb812323359a83d
Tushar Chenan <tusky2002@gmail.com>
2023-04-30 15:20:54 +0530

    Add small test case for flow

--- src/ford-fulkerson.cpp
+++ src/ford-fulkerson.cpp
@@ -1,15 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Graph {
-
 public:
     int n;
     bool directed = false;
     Graph();
     Graph(int n);
     map<int, set<int>> adj;
-    map<pair<int,int>, int> cap;
+    map<pair<int, int>, int> cap;
     void readEdge(string filename, bool directed, bool weighted, bool oneBased);
     void addDirectedEdge(int x, int y, int c);
     void addUndirectedEdge(int x, int y, int c);
@@ -18,118 +17,98 @@ public:
     int getCapacity(int x, int y);
 };
 
-Graph::Graph()
-{
-    n=0;
+Graph::Graph() {
+    n = 0;
 }
 
-Graph::Graph(int n)
-{
+Graph::Graph(int n) {
     this->n = n;
 }
 
-int Graph::getCapacity(int x, int y)
-{
-    if (adj[x].find(y)==adj[x].end())
-        return 0;
+int Graph::getCapacity(int x, int y) {
+    if (adj[x].find(y) == adj[x].end()) return 0;
 
-    return cap[{x,y}];
+    return cap[{x, y}];
 }
 
-void Graph::addDirectedEdge(int x, int y, int c)
-{
-    if (adj[x].find(y)==adj[x].end())
-    {
+void Graph::addDirectedEdge(int x, int y, int c) {
+    if (adj[x].find(y) == adj[x].end()) {
         adj[x].insert(y);
-        cap[{x,y}]=c;
-    }
-    else
-        cap[{x,y}]+=c;
+        cap[{x, y}] = c;
+    } else
+        cap[{x, y}] += c;
 }
 
-void Graph::addUndirectedEdge(int x, int y, int c)
-{
-    addDirectedEdge(x,y,c);
-    addDirectedEdge(y,x,c);
+void Graph::addUndirectedEdge(int x, int y, int c) {
+    addDirectedEdge(x, y, c);
+    addDirectedEdge(y, x, c);
 }
 
-void Graph::readEdge(string filename, bool directed, bool weighted, bool oneBased)
-{
+void Graph::readEdge(string filename, bool directed, bool weighted, bool oneBased) {
     ifstream cin;
     cin.open(filename);
-    cin>>n;
+    cin >> n;
 
     int m;
-    cin>>m;
+    cin >> m;
 
-    while (m--)
-    {
-        int x,y,c;
-        cin>>x>>y;
+    while (m--) {
+        int x, y, c;
+        cin >> x >> y;
 
         if (weighted)
-            cin>>c;
+            cin >> c;
         else
-            c=1;
+            c = 1;
 
-        if (oneBased)        
-        {
+        if (oneBased) {
             x--;
             y--;
         }
 
         if (directed)
-            addDirectedEdge(x,y,c);
+            addDirectedEdge(x, y, c);
         else
-            addUndirectedEdge(x,y,c);
+            addUndirectedEdge(x, y, c);
     }
 }
 
-void Graph::getPath(int start, int end, vector<int>& path, vector<int>& seen)
-{
-    if (seen[start])
-        return;
-    cout << start << ' ' << end << endl;
+void Graph::getPath(int start, int end, vector<int>& path, vector<int>& seen) {
+    if (seen[start]) return;
 
-    seen[start]=1;
+    seen[start] = 1;
 
-    if (start==end)
-    {
+    if (start == end) {
         path.push_back(end);
         return;
     }
 
-    for (auto p : adj[start])
-    {
-        getPath(p, end, path,seen);
+    for (auto p : adj[start]) {
+        getPath(p, end, path, seen);
 
-        if (!path.empty())
-        {
+        if (!path.empty()) {
             path.push_back(start);
             return;
         }
     }
 }
 
-void Graph::dfs(int node, vector<int>& seen)
-{
-    if (seen[node])
-        return;
+void Graph::dfs(int node, vector<int>& seen) {
+    assert(0 <= node && node < n);
 
-    seen[node]=1;
+    if (seen[node]) return;
 
-    for (auto p : adj[node])
-        dfs(p, seen);
+    seen[node] = 1;
+
+    for (auto p : adj[node]) dfs(p, seen);
 }
 
-class Flow
-{
-
+class Flow {
 private:
-    int s,t;
+    int s, t;
     Graph g;
-    map<pair<int,int>, int> flow;
-    map<pair<int,int>,int> fordFulkerson();
+    map<pair<int, int>, int> flow;
+    map<pair<int, int>, int> fordFulkerson();
     Graph buildResidualGraph();
     bool isForward(int x, int y);
     int getFlow(int x, int y);
@@ -138,164 +117,129 @@ private:
 
 public:
     Flow(Graph g, int s, int t);
-    map<pair<int,int>,int> maxFlow();
+    map<pair<int, int>, int> maxFlow();
     vector<int> stCut();
 };
 
-Flow::Flow(Graph g, int s, int t)
-{
+Flow::Flow(Graph g, int s, int t) {
     this->s = s;
     this->t = t;
     this->g = g;
 
-    for (int i=0; i<g.n; i++)
-    {
-        for (auto p : g.adj[i])
-            flow[{i,p}]=0;
+    for (int i = 0; i < g.n; i++) {
+        for (auto p : g.adj[i]) flow[{i, p}] = 0;
     }
 }
 
-bool Flow::isForward(int x, int y)
-{
-    if (g.adj[x].find(y)==g.adj[x].end())
-        return false;
+bool Flow::isForward(int x, int y) {
+    if (g.adj[x].find(y) == g.adj[x].end()) return false;
 
     return true;
 }
 
-int Flow::getFlow(int x, int y)
-{
-    if (g.adj[x].find(y)==g.adj[x].end())
-        return 0;
-    
-    return flow[{x,y}];
+int Flow::getFlow(int x, int y) {
+    if (g.adj[x].find(y) == g.adj[x].end()) return 0;
+
+    return flow[{x, y}];
 }
 
-void Flow::updateFlow(int x, int y, int f)
-{
-    flow[{x,y}]=f;
+void Flow::updateFlow(int x, int y, int f) {
+    flow[{x, y}] = f;
 }
 
-Graph Flow::buildResidualGraph()
-{
+Graph Flow::buildResidualGraph() {
     Graph res(g.n);
-    
-    for (int i=0; i<g.n; i++)
-    {
-        for (auto j : g.adj[i])
-        {
-            int c = g.getCapacity(i,j);
-            int f = getFlow(i,j);
 
-            if (f<c)
-                res.addDirectedEdge(i,j,c-f);
+    for (int i = 0; i < g.n; i++) {
+        for (auto j : g.adj[i]) {
+            int c = g.getCapacity(i, j);
+            int f = getFlow(i, j);
 
-            if (f>0)
-                res.addDirectedEdge(j,i,f);
+            if (f < c) res.addDirectedEdge(i, j, c - f);
+
+            if (f > 0) res.addDirectedEdge(j, i, f);
         }
     }
 
     return res;
 }
 
-void Flow::augment(vector<int> path, Graph res)
-{
+void Flow::augment(vector<int> path, Graph res) {
     int b = INT_MAX;
 
-    for (int i=0; i<path.size()-1; i++)
-        b = min(b, res.getCapacity(path[i+1],path[i]));
+    for (int i = 0; i < path.size() - 1; i++) b = min(b, res.getCapacity(path[i + 1], path[i]));
 
-    for (int i=0; i<path.size()-1; i++)
-    {
-        int x = path[i+1];
+    for (int i = 0; i < path.size() - 1; i++) {
+        int x = path[i + 1];
         int y = path[i];
 
-        if (isForward(x,y))
-        {
-            int f = getFlow(x,y);
-            updateFlow(x,y,f+b);
-        }
-        else
-        {
-            int f = getFlow(y,x);
-            updateFlow(y,x,f-b);
+        if (isForward(x, y)) {
+            int f = getFlow(x, y);
+            updateFlow(x, y, f + b);
+        } else {
+            int f = getFlow(y, x);
+            updateFlow(y, x, f - b);
         }
     }
 }
 
-map<pair<int,int>,int> Flow::fordFulkerson()
-{
+map<pair<int, int>, int> Flow::fordFulkerson() {
     Graph res = buildResidualGraph();
     vector<int> path;
-    vector<int> seen(g.n,0);
-    res.getPath(s,t,path,seen);
+    vector<int> seen(g.n, 0);
+    res.getPath(s, t, path, seen);
 
-    if (path.size()==0)
-        return flow;
+    if (path.size() == 0) return flow;
 
     int f = 0;
 
-    for (auto x : g.adj[0])
-        f+=flow[{0,x}];
+    for (auto x : g.adj[0]) f += flow[{0, x}];
 
-    cout<<f<<"\n";
-
-    augment(path,res);
+    augment(path, res);
     return fordFulkerson();
 }
 
-map<pair<int,int>,int> Flow::maxFlow()
-{
+map<pair<int, int>, int> Flow::maxFlow() {
     return fordFulkerson();
 }
 
-vector<int> Flow::stCut()
-{
+vector<int> Flow::stCut() {
     Graph res = buildResidualGraph();
-    vector<int> seen(res.n,0);
-    res.dfs(s,seen);
+    vector<int> seen(res.n, 0);
+    res.dfs(s, seen);
     return seen;
 }
 
-class Bipartite
-{
-
+class Bipartite {
 private:
     Graph g;
     vector<int> partition;
 
 public:
     bool twoColoring(vector<int>& col);
-    vector<pair<int,int>> maximumMatching();
+    vector<pair<int, int>> maximumMatching();
 };
 
-bool Bipartite::twoColoring(vector<int>& col)
-{
+bool Bipartite::twoColoring(vector<int>& col) {
     col.resize(g.n, -1);
-    col[0]=0;
-    
+    col[0] = 0;
+
     vector<int> v;
     v.push_back(0);
 
-    while (v.size())
-    {
-        int x = v[v.size()-1];
+    while (v.size()) {
+        int x = v[v.size() - 1];
         v.pop_back();
         int c = col[x];
 
-        for (auto p : g.adj[x])
-        {
+        for (auto p : g.adj[x]) {
             int y = p;
 
-            if (col[y]==-1)
-            {
-                col[y] = 1-col[x];
+            if (col[y] == -1) {
+                col[y] = 1 - col[x];
                 v.push_back(y);
-            }
-            else
-            {
-                if (col[y]==col[x])
-                    return false;
+            } else {
+                if (col[y] == col[x]) return false;
             }
         }
     }
@@ -303,86 +247,73 @@ bool Bipartite::twoColoring(vector<int>& col)
     return true;
 }
 
-vector<pair<int,int>> Bipartite::maximumMatching()
-{
+vector<pair<int, int>> Bipartite::maximumMatching() {
     vector<int> col;
-    vector<pair<int,int>> res;
+    vector<pair<int, int>> res;
 
-    if (!twoColoring(col))
-    {
-        res.push_back({-1,-1});
+    if (!twoColoring(col)) {
+        res.push_back({-1, -1});
         return res;
     }
-        
+
     int s = g.n + 1;
-    int t = s+1;
+    int t = s + 1;
 
     Graph b(g);
 
-    b.n+=2;
-    vector<pair<int,int>> temp;
+    b.n += 2;
+    vector<pair<int, int>> temp;
 
-    for (int i=0; i<g.n; i++)
-    {
-        if (col[i]==0)
-            b.addDirectedEdge(s,i,1);
+    for (int i = 0; i < g.n; i++) {
+        if (col[i] == 0)
+            b.addDirectedEdge(s, i, 1);
         else
-            b.addDirectedEdge(i,t,1);
+            b.addDirectedEdge(i, t, 1);
     }
 
-    Flow f(b,s,t);
-    map<pair<int,int>,int> flow = f.maxFlow();
+    Flow f(b, s, t);
+    map<pair<int, int>, int> flow = f.maxFlow();
 
-    for (auto x : flow)
-    {
-        if (x.second > 0)
-        {
-            pair<int,int> p = x.first;
+    for (auto x : flow) {
+        if (x.second > 0) {
+            pair<int, int> p = x.first;
 
-            if (p.first<g.n && p.second<g.n)
-                res.push_back(p);
+            if (p.first < g.n && p.second < g.n) res.push_back(p);
         }
     }
     return res;
 }
 
-void runFordFulkerson(string filename)
-{
+void runFordFulkerson(string filename) {
     Graph g;
-    g.readEdge(filename,true,false,true);
+    g.readEdge(filename, false, true, true);
 
-    Flow f(g,0,g.n-1);
-    map<pair<int,int>,int> flow = f.maxFlow();
+    Flow f(g, 0, g.n - 1);
+    map<pair<int, int>, int> flow = f.maxFlow();
 
-    int c=0;
+    int c = 0;
 
-    for (auto p : g.adj[0])
-        c+=flow[{0,p}];
+    for (auto p : g.adj[0]) c += flow[{0, p}];
 
-    cout<<"The max flow is : "<<c<<"\n";
+    cout << "The max flow is : " << c << "\n";
 
     vector<int> cut = f.stCut();
 
-    cout<<"ST cut\n";
+    cout << "ST cut\n";
 
-    for (int i=0; i<cut.size(); i++)
-    {
-        if (cut[i]==1)
-            cout<<i<<" ";
+    for (int i = 0; i < cut.size(); i++) {
+        if (cut[i] == 1) cout << i << " ";
     }
 
-    cout<<"\n";
+    cout << "\n";
 
-    for (int i=0; i<cut.size(); i++)
-    {
-        if (cut[i]==0)
-            cout<<i<<" ";
+    for (int i = 0; i < cut.size(); i++) {
+        if (cut[i] == 0) cout << i << " ";
     }
 
-    cout<<"\n";
+    cout << "\n";
 }
 
-int32_t main(int32_t argc, char** argv)
-{
+int main(int argc, char** argv) {
     runFordFulkerson(argv[1]);
 }
